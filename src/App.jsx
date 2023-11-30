5c1cd2b76d1208dbeb2243364eb76fdf4401b57d
RHETT CHASE <rhettjchase@gmail.com>
2023-11-30 11:59:51 -0800

    create HornForm structure

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Gallery from './components/Gallery'
+import Form from './components/HornForm'
 import jsonData from './assets/HornedBeast.json'
+import HornForm from './components/HornForm';
 
 
 function App() {
@@ -12,6 +14,7 @@ function App() {
     <>
       <Header title="Gallery of Horns" />
       <Gallery jsonData={jsonData} />
+      <HornForm />
       <Footer author="Rhett Chase"/>
     </>
   );
